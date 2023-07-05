68267a2926e8fec6e66b19824fc3bdcbd5fd098f
Jelani <lylesjelani@gmail.com>
2023-07-05 18:24:30 -0400

    not much luck on a solution

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const 
+const express = require('express');
+// const router = express.Router();
+const config = require('config');
+const connectDB = require('./config/db');
+
+
+const createNewShort = url => {
+
+    connectDB();
+
+    let obj = {
+        longUrl: url
+    }
+
+    fetch('http://localhost:3000/api/url/shorten', {
+        Method: 'POST',
+        Headers: {
+            Accept: 'application.json', 'Content-Type': 'application/json'
+        },
+        Body: obj,
+        Cache: 'default'
+    })
+}
+
+module.exports = createNewShort;
+
+// createNewShort('https://www.topcoder.com/thrive/articles/fetch-api-javascript-how-to-make-get-and-post-requests');
